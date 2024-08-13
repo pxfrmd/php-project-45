612ead32eb5ed0d9e0a5aa36e66ab239c832e6c7
Evgenii Vlasov <senzik@gmail.com>
2024-08-13 18:59:17 +0300

    added PHPCodeSniffer

--- src/Cli.php
+++ src/Cli.php
@@ -5,9 +5,9 @@ namespace BrainGames\Cli;
 use function Cli\line;
 use function Cli\prompt;
 
-function welcome()
+function welcome(): void
 {
-line('Welcome to the Brain Game!');
-$name = prompt('May I have your name?');
-line("Hello, %s!", $name);
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
 }
