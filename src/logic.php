834164d04ac082355c2714ce3dffb74443119068
Evgenii Vlasov <senzik@gmail.com>
2024-08-15 13:38:16 +0300

    bug fixes, game is works now

--- src/logic.php
+++ src/logic.php
@@ -3,42 +3,43 @@ namespace logic;
 use function Cli\line;
 use function Cli\prompt;
 
-$name;
-
-function welcome(): void
+$name = "";
+$count = 0;
+function welcome(): string
 {
-    line('Welcome to the Brain Game!');
     global $name;
+    line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
+    return $name;
 }
 
-$count = 0;
 
-while ($count < 3) {
 function brainEven(): void
 {   
-    $randomNumber = rand(0, 100);
-    echo "Answer \"yes\" if the number is even, otherwise answer \"no\".";
-    echo "Question: {$randomNumber}";
-    $typeOfRandomNumberIsEven = "yes";
-    if ($typeOfRandomNumberIsEven % 2 !=0) {
-        $typeOfRandomNumberIsEven = "no";
-    }
-    $answer = prompt("Your answer:");
-    $answerCleaned = strtolower($answer).trim($answer);
-    if ($typeOfRandomNumberIsEven != $answerCleaned) {
-        echo "\"{$answerCleaned}\" is wrong answer ;(. Correct answer was \"{$typeOfRandomNumberIsEven}\". Let's try again, Bill!";
-        global $count;
-        global $name;
-        $count = 3;
-    } else {
-        echo "Correct!";
-        $count++;
-        if ($count == 3) {
-            echo "Congratulations, {$name}!";
+    global $count;
+    global $name;
+    while ($count < 3) {
+        $randomNumber = rand(1, 100);
+        echo "Answer \"yes\" if the number is even, otherwise answer \"no\".\n";
+        echo "Question: {$randomNumber}\n";
+        $typeOfRandomNumberIsEven = "yes";
+        if ($randomNumber % 2 !=0) {
+            $typeOfRandomNumberIsEven = "no";
+        }
+        $answer = prompt("Your answer");
+        $answerCleaned = strtolower($answer);
+        if ($typeOfRandomNumberIsEven != $answerCleaned) {
+            echo "\"{$answerCleaned}\" is wrong answer ;(. Correct answer was \"{$typeOfRandomNumberIsEven}\". Let's try again, {$name}\n";
+            $count = 3;
+        } else {
+            echo "Correct!\n";
+            $count++;
+            if ($count == 3) {
+                echo "Congratulations, {$name}!\n";
+            }
         }
     }
 }
-}
-
+welcome();
+brainEven();
